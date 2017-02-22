3eff9f9825e2836c8a123121a6d39999409f20cc
Sunny NG <ngsanluk@gmail.com>
2017-02-22 23:48:34 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# A simple demo iOS app that creat Instagram like photo filter
+A simple iOS app that demonstrates how to apply CIFilter (Core Image Filter - a library provided by Apple) to an UIImage object in your iOS app.
--- UsingPhotoFiliter/ViewController.swift
+++ UsingPhotoFiliter/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var image: UIImage!
+    
+    
+    @IBAction func applyFilter(_ sender: UISlider) {
+        let effectValue = sender.value
+        print("\(effectValue)")
+        
+        let context = CIContext(options: nil)
+        let cImage = CIImage(cgImage: self.image.cgImage!)
+        
+        let sepiaTone = CIFilter(name: "CISepiaTone")
+        sepiaTone?.setValue(cImage, forKey: "inputImage")
+        
+        
+        sepiaTone?.setValue(effectValue, forKey: "inputIntensity")
+        let result = sepiaTone?.value(forKey: "outputImage") as! CIImage
+        
+        let imageRef = context.createCGImage(result, from: CGRect(x: 0, y: 0, width: self.imageView.image!.size.width, height: self.imageView.image!.size.height))
+        
+        let image =  UIImage(cgImage: imageRef!)
+        
+        self.imageView.image = image
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.image = imageView.image
     }
 
     override func didReceiveMemoryWarning() {
