6ec85ca6304ea5b02e4a27bcce8fb9501b0f6a1c
Sunny NG <ngsanluk@gmail.com>
2017-02-22 23:04:55 +0800

    Initial Commit

---
+++ UsingPhotoFiliter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingPhotoFiliter
+//
+//  Created by Sunny NG on 22/2/2017.
+//  Copyright © 2017 Sunny NG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
